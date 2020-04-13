be401f768a3e14a3b7c770bec05ab5b21c9cd677
Ilya Dymshits <ilya@devlion.co>
2020-04-13 10:05:36 +0300

    mod (update) add mods bbb mindmap sharedgeogebra whiteboard

---
+++ mod/whiteboard/lang/he/whiteboard.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_whiteboard
+ * @category    string
+ * @copyright   2019 Devlion <info@devlion.co>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'לוח שיתופי';
+$string['pluginname'] = 'לוח שיתופי';
+$string['whiteboardname'] = 'שם הלוח';
+$string['whiteboardname_help'] = 'שם הפעילות';
+$string['whiteboardsettings'] = 'הגדרות פעילות';
+$string['whiteboardfieldset'] = 'תצוגה';
+$string['type'] = 'סוג הלוח';
+$string['pluginadministration'] = 'ניהול והגדרות הלוח';
+
+$string['whiteboard_token'] = 'קוד השירות';
+$string['whiteboard_token_desc'] = 'תיאור קוד השירות';
+$string['whiteboard_url'] = 'כתובת לוח שיתופי';
+$string['whiteboard_url_desc'] = 'תיאור כתובת לוח שיתופי';
+
+$string['openpopup'] = 'התחל לוח שיתופי';
+$string['newwindow'] = 'התחל לוח שיתופי';
+$string['directlink'] = 'התחל לוח שיתופי';
+$string['modulenameplural'] = 'לוחות שיתופיים';
+$string['nonewmodules'] = 'אין מודולים חדשים';
+
+$string['type_iframe'] = 'Iframe';
+$string['type_window'] = 'חלון חדש';
+$string['type_popup'] = 'פתיחה בחלון חדש';
+$string['type_link'] = 'קישור';
+
+$string['curlerror'] = 'ארעה שגיאת שרת: {$a}';
+$string['missingidandcmid'] = 'חסר פרמטר id או cmid';
+
+$string['privacy:metadata'] = 'תוסף לוח שיתופי אינו מכיל מידע אישי';
+
+$string['whiteboard:addinstance'] = 'הוסף לוח שיתופי';
+$string['whiteboard:view'] = 'מבט לוח שיתופי';
